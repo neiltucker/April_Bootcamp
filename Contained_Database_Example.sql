2ab9f96659fbf92f25c5fd548e4dd77406f7ad4c
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2022-04-25 08:42:52 -0400

    Add files via upload

---
+++ Contained_Database_Example.sql
@@ -0,0 +1,22 @@
+USE master
+GO
+EXEC sp_configure 'show advanced', 1
+GO
+RECONFIGURE
+GO
+EXEC sp_configure 'contained database authentication', 1
+GO
+RECONFIGURE
+GO
+ 
+SELECT containment,name FROM sys.databases
+GO
+
+USE [Demo6]
+GO
+
+CREATE USER User1 WITH PASSWORD = 'Pa$$w0rd'
+GO
+
+ALTER ROLE [db_datareader] ADD MEMBER [User1]
+GO
